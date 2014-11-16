95da9c908398ee1447608b4dc831be0c4ce91c52
Roywcm <info@roywulms.nl>
2014-11-16 13:17:03 +0100

    updated formulae

--- bower_components/jquery-formulae/dist/jquery.formulae.js
+++ bower_components/jquery-formulae/dist/jquery.formulae.js
@@ -26,9 +26,7 @@
                 math: [
                     {"label": "<span class=\"glyphicon glyphicon-plus\"></span>", "value": "+"},
                     {"label": "<span class=\"glyphicon glyphicon-minus\"></span>", "value": "-"},
-                    {"label": "<span class=\"glyphicon glyphicon-remove\"></span>", "value": "*"},
-                    {"label": "(", "value": "("},
-                    {"label": ")", "value": ")"}
+                    {"label": "<span class=\"glyphicon glyphicon-remove\"></span>", "value": "*"}
                 ]
             };
 
